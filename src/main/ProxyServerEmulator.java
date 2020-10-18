b66f5679268b005f2c0d80cb452a215de1380ad4
bv <bv@infra.guru>
2020-10-18 18:21:16 +0300

    Comment is corrected

--- src/main/ProxyServerEmulator.java
+++ src/main/ProxyServerEmulator.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 /**
  * To keep data storing in case when the proxy stops itself, we may use storing to files. Each server handler thread may write the received data
  * to its own file (i.e. file with certain number) together with adding the data to the sender's queue. At next step, the sender sends data to the server and if 200 status
- * is received, offset is incremented and stored in the other file of the thread which put this data to the sender's queue.
+ * is received, offset is incremented and stored in the other file of the thread which put this data to the sender's queue. (We add offset to data structure as well)
  * Then, if proxy downs for some reason, it starts and loads data from files, puts to the sender's queue data messages which have no committed offset (per each thread).
  * Technically, it just compares data file's lines numbers and last committed offset for the current thread
  * Background thread may remove lines with numbers which are not more than last committed offset from data files with some retention policy
